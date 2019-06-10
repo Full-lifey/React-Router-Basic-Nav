59227c8c80f1f7f57d291042951ddd276098d5ff
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-06-10 14:29:58 -0600

    MVP: finished all steps to implement navigation routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact to='/' component={Home} />
+    <Route to='/about' component={About} />
+    <Route to='/contact' component={Contact} />
   </div>
 );
 
